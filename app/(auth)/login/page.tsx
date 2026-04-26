55e71713f86b4ad5c0dc6e1205024ea5bea3c809
arnaudvlb <arnaud.vanlerberghe0@gmail.com>
2026-04-26 20:22:04 +0200

    Chore Gestion message erreur login/register

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import AuthForm from "@/components/AuthForm/AuthForm";
 import { useLogin } from "@/hooks/useLogin";
+import FormMessage from "@/components/ui/FormMessage/FormMessage";
 
 export default function LoginPage() {
+  const [message, setMessage] = useState("");
   const { loginUser, loading, error } = useLogin();
   const router = useRouter();
 
@@ -16,28 +19,34 @@ export default function LoginPage() {
     });
 
     if (res) {
-      router.push("/resources");
+      setMessage("Connexion réussie !");
+      setTimeout(() => {
+        router.push("/resources");
+      }, 1500);
     }
-
-    if (error) return <p>Erreur : {error}</p>;
   };
 
   return (
-    <AuthForm
-      titreForm="Connexion"
-      champs={["Email", "Mot de passe"]}
-      names={["email", "password"]}
-      buttonText={loading ? "Connexion..." : "Se connecter"}
-      placeholders={["exemple@email.com", "••••••••"]}
-      onSubmit={handleSubmit}
-      footerContent={
-        <>
-          <span>Pas encore de compte ? </span>
-          <Link href="/register" className="link">
-            Créer un compte
-          </Link>
-        </>
-      }
-    />
+    <>
+      {(message || error) && (
+        <FormMessage message={message || error || ""} error={!!error} />
+      )}
+      <AuthForm
+        titreForm="Connexion"
+        champs={["Email", "Mot de passe"]}
+        names={["email", "password"]}
+        buttonText={loading ? "Connexion..." : "Se connecter"}
+        placeholders={["exemple@email.com", "••••••••"]}
+        onSubmit={handleSubmit}
+        footerContent={
+          <>
+            <span>Pas encore de compte ? </span>
+            <Link href="/register" className="link">
+              Créer un compte
+            </Link>
+          </>
+        }
+      />
+    </>
   );
 }
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -3,43 +3,56 @@
 import { useRouter } from "next/navigation";
 import AuthForm from "@/components/AuthForm/AuthForm";
 import { useRegister } from "@/hooks/useRegister";
+import FormMessage from "@/components/ui/FormMessage/FormMessage";
 
 export default function RegisterPage() {
-  const { registerUser, loading, error } = useRegister();
+  const { registerUser, loading, error, data } = useRegister();
   const router = useRouter();
 
   const handleSubmit = async (formData: Record<string, string>) => {
-  const res = await registerUser({
-    email: formData.email,
-    password: formData.password,
-    nom: formData.nom,
-    prenom: formData.prenom,
-    telephone: formData.telephone,
-    pseudo: formData.pseudo,
-  });
+    const res = await registerUser({
+      email: formData.email,
+      password: formData.password,
+      nom: formData.nom,
+      prenom: formData.prenom,
+      telephone: formData.telephone,
+      pseudo: formData.pseudo,
+    });
 
-  if (res) {
-    router.push("/login");
-  }
-
-  if (error) return <p>Erreur : {error}</p>;
-};
+    if (res) {
+      setTimeout(() => {
+        router.push("/login");
+      }, 1500);
+    }
+  };
 
   return (
-    <AuthForm
-      titreForm="Créer un compte"
-      champs={["Prénom", "Nom", "Pseudo", "Email", "Téléphone", "Mot de passe"]}
-      names={["prenom", "nom", "pseudo", "email", "telephone", "password"]}
-      buttonText={loading ? "Inscription..." : "S'inscrire"}
-      placeholders={[
-        "Prénom",
-        "Nom",
-        "Pseudo",
-        "exemple@email.com",
-        "0612345678",
-        "••••••••"
-      ]}
-      onSubmit={handleSubmit}
-    />
+    <>
+      {(data?.message || error) && (
+        <FormMessage message={data?.message || error || ""} error={!!error} />
+      )}
+      <AuthForm
+        titreForm="Créer un compte"
+        champs={[
+          "Prénom",
+          "Nom",
+          "Pseudo",
+          "Email",
+          "Téléphone",
+          "Mot de passe",
+        ]}
+        names={["prenom", "nom", "pseudo", "email", "telephone", "password"]}
+        buttonText={loading ? "Inscription..." : "S'inscrire"}
+        placeholders={[
+          "Prénom",
+          "Nom",
+          "Pseudo",
+          "exemple@email.com",
+          "0612345678",
+          "••••••••",
+        ]}
+        onSubmit={handleSubmit}
+      />
+    </>
   );
 }
