ddef5e52a5dfd273a0a53946aea07552a742d434
arnaudvlb <arnaud.vanlerberghe0@gmail.com>
2026-08-16 18:47:00 +0200

    Fix CSP et intégration dans un proxy

--- next.config.ts
+++ next.config.ts
@@ -31,11 +31,6 @@ const nextConfig: import("next").NextConfig = {
             key: "Permissions-Policy",
             value: "camera=(), microphone=(), geolocation=()",
           },
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; connect-src 'self'; form-action 'self'; frame-ancestors 'none';",
-          },
         ],
       },
     ];
