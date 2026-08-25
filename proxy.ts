3e8648d271c1baf63315b2e2a42a60c340bb5d67
Andrea Pastourel <a.pastourel@outlook.com>
2026-08-25 11:53:18 +0200

    Secu/ ajout d'une page de maintenance

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function proxy(request: NextRequest) {
+  const maintenanceMode =
+    process.env.MAINTENANCE_MODE === "true";
+
+  const isMaintenancePage =
+    request.nextUrl.pathname === "/maintenance";
+
+  if (maintenanceMode && !isMaintenancePage) {
+    return NextResponse.redirect(
+      new URL("/maintenance", request.url)
+    );
+  }
+
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
 
   const csp = [
